51bc24175c4708ca38d38f8d2b413c8db9c43874
Fabio <fabio.wow.lol@gmail.com>
2022-06-21 18:30:59 -0300

    componentes da pagina

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,11 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe Now
+    </button>
+  );
+}
